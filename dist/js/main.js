15f81386e76dad5429c2e0f2ce6bc2871b259618
coffmanjrp <coffmanjrp@gmail.com>
2021-01-01 12:18:46 -0800

    first commit

---
+++ dist/js/main.js
@@ -0,0 +1,65 @@
+"use strict";
+var resultEl = document.getElementById('result');
+var lengthEl = document.getElementById('length');
+var uppercaseEl = document.getElementById('uppercase');
+var lowercaseEl = document.getElementById('lowercase');
+var numbersEl = document.getElementById('numbers');
+var symbolsEl = document.getElementById('symbols');
+var generateEl = document.getElementById('generate');
+var clipboardEl = document.getElementById('clipboard');
+function getRandomLower() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+function getRandomUpper() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+function getRandomNumber() {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+function getRandomSymbol() {
+    var symbols = '!@#$%^&*(){}[]=<>/,.';
+    return symbols[Math.floor(Math.random() * symbols.length)];
+}
+var randomFunc = {
+    lower: getRandomLower,
+    upper: getRandomUpper,
+    number: getRandomNumber,
+    symbol: getRandomSymbol,
+};
+clipboardEl.addEventListener('click', function () {
+    var textarea = document.createElement('textarea');
+    var password = resultEl.innerText;
+    if (!password) {
+        return;
+    }
+    textarea.value = password;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    textarea.remove();
+    alert('Password copied to clipboard!');
+});
+function generatePassword(lower, upper, number, symbol, length) {
+    var generatedPassword = '';
+    var typesCount = lower + upper + number + symbol;
+    var typesArr = [{ lower: lower }, { upper: upper }, { number: number }, { symbol: symbol }].filter(function (item) { return Object.values(item)[0]; });
+    if (typesCount === 0) {
+        return '';
+    }
+    for (var i = 0; i < length; i += typesCount) {
+        typesArr.forEach(function (type) {
+            var funcName = Object.keys(type)[0];
+            generatedPassword += randomFunc[funcName]();
+        });
+    }
+    var finalPassword = generatedPassword.slice(0, length);
+    return finalPassword;
+}
+generateEl.addEventListener('click', function () {
+    var length = +lengthEl.value;
+    var hasLower = lowercaseEl.checked;
+    var hasUpper = uppercaseEl.checked;
+    var hasNumber = numbersEl.checked;
+    var hasSymbol = symbolsEl.checked;
+    resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length);
+});
---
+++ src/ts/main.ts
@@ -0,0 +1,87 @@
+const resultEl = document.getElementById('result') as HTMLSpanElement;
+const lengthEl = document.getElementById('length') as HTMLLabelElement;
+const uppercaseEl = document.getElementById('uppercase') as HTMLInputElement;
+const lowercaseEl = document.getElementById('lowercase') as HTMLInputElement;
+const numbersEl = document.getElementById('numbers') as HTMLInputElement;
+const symbolsEl = document.getElementById('symbols') as HTMLInputElement;
+const generateEl = document.getElementById('generate') as HTMLButtonElement;
+const clipboardEl = document.getElementById('clipboard') as HTMLButtonElement;
+
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomSymbol() {
+  const symbols = '!@#$%^&*(){}[]=<>/,.';
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+const randomFunc = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  symbol: getRandomSymbol,
+};
+
+clipboardEl.addEventListener('click', () => {
+  const textarea = document.createElement('textarea');
+  const password = resultEl.innerText;
+
+  if (!password) {
+    return;
+  }
+
+  textarea.value = password;
+  document.body.appendChild(textarea);
+  textarea.select();
+  document.execCommand('copy');
+  textarea.remove();
+  alert('Password copied to clipboard!');
+});
+
+function generatePassword(lower, upper, number, symbol, length) {
+  let generatedPassword = '';
+  const typesCount = lower + upper + number + symbol;
+  const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter(
+    (item) => Object.values(item)[0]
+  );
+
+  if (typesCount === 0) {
+    return '';
+  }
+
+  for (let i = 0; i < length; i += typesCount) {
+    typesArr.forEach((type) => {
+      const funcName = Object.keys(type)[0];
+      generatedPassword += randomFunc[funcName]();
+    });
+  }
+
+  const finalPassword = generatedPassword.slice(0, length);
+
+  return finalPassword;
+}
+
+generateEl.addEventListener('click', () => {
+  const length = +lengthEl.value;
+  const hasLower = lowercaseEl.checked;
+  const hasUpper = uppercaseEl.checked;
+  const hasNumber = numbersEl.checked;
+  const hasSymbol = symbolsEl.checked;
+
+  resultEl.innerText = generatePassword(
+    hasLower,
+    hasUpper,
+    hasNumber,
+    hasSymbol,
+    length
+  );
+});
